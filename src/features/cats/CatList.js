4724b67ba1bcdc7184dd9db9e7616e3e1c8001b4
avcibatuhan <avcibatuhan26@gmail.com>
2022-03-03 09:52:55 +0300

    redux thunk lab

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,14 @@
 // write your CatList component here
+import React from 'react'
+
+export default function CatList({catPics}) {
+    console.log(catPics)
+  return (
+    <div><ul>
+        {catPics.map((cat) => (
+        <li key={cat.id} ><img  src={cat.url} alt="cat"/></li>
+    ))}
+    </ul>
+    </div>
+  )
+}
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,9 +1,20 @@
 import React from "react";
-
+import {useSelector,useDispatch} from "react-redux";
+import {useEffect} from "react";
+import { fetchCats } from "./catsSlice";
+import CatList from "./CatList";
 function Cats() {
+  const catPics = useSelector((state) => state.entities);
+  const dispatch = useDispatch();
+
+  useEffect(()=> {
+    dispatch(fetchCats())
+  },[])
+ 
   return (
     <div>
       <h1>CatBook</h1>
+      <CatList catPics={catPics}/>
       {/* add CatList component here */}
     </div>
   );
